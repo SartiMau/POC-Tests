aba88838f3e1a7e069fd708337f4bff534b68cd6
Mariano <84336381+mariano98montero@users.noreply.github.com>
2022-11-30 17:45:32 -0300

    Added coroutine testing to kotlin unit tests

--- kotlin_counter/src/test/java/com/example/kotlin_counter/viewmodeltest/CounterKotlinUnitTest.kt
+++ kotlin_counter/src/test/java/com/example/kotlin_counter/viewmodeltest/CounterKotlinUnitTest.kt
@@ -2,22 +2,22 @@ package com.example.kotlin_counter.viewmodeltest
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import com.example.kotlin_counter.mvvm.model.KotlinCounterModel
-import com.example.kotlin_counter.mvvm.viewmodel.KotlinCounterViewModel
-import com.example.kotlin_counter.mvvm.viewmodel.KotlinCounterViewModel.KotlinCounterState.INIT
-import com.example.kotlin_counter.mvvm.viewmodel.KotlinCounterViewModel.KotlinCounterState.UPDATE_VALUE
+import com.example.kotlin_counter.data.CountData
+import com.example.kotlin_counter.viewmodel.KotlinCounterViewModel
+import com.example.kotlin_counter.viewmodel.KotlinCounterViewModel.KotlinCounterState.INIT
+import com.example.kotlin_counter.viewmodel.KotlinCounterViewModel.KotlinCounterState.UPDATE_VALUE
 import io.mockk.MockKAnnotations
-import io.mockk.every
+import io.mockk.coVerify
 import io.mockk.mockk
-import io.mockk.mockkClass
 import io.mockk.unmockkAll
 import io.mockk.verify
 import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Before
@@ -33,22 +33,21 @@ class CounterKotlinUnitTest {
 
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
-    private val testDispatcher = TestCoroutineDispatcher()
 
     private lateinit var viewModel: KotlinCounterViewModel
 
     private val mockedObserver = mockk<Observer<KotlinCounterViewModel.KotlinCounterData>>(relaxed = true)
 
     // This is just here to show how would you mock a class with MockK
-    private val mockedModel: KotlinCounterModel = mockkClass(KotlinCounterModel::class)
+    // private val mockedDataBase: CountData = mockkClass(CountData::class)
 
-    private val model = KotlinCounterModel(ZERO_INT)
+    private val dataBase = CountData(ZERO_INT)
 
     @Before
     fun setup() {
-        Dispatchers.setMain(testDispatcher)
-        MockKAnnotations.init()
-        viewModel = KotlinCounterViewModel(model)
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        MockKAnnotations.init(this, relaxed = true)
+        viewModel = KotlinCounterViewModel()
     }
 
     @After
@@ -57,111 +56,98 @@ class CounterKotlinUnitTest {
         unmockkAll()
     }
 
-    //This test is only for demonstration purposes.
-    @Test
-    fun `Mocked model test`() {
-        val list = arrayListOf<KotlinCounterViewModel.KotlinCounterData>()
-        val secondaryViewModel = KotlinCounterViewModel(mockedModel)
-
-        every { mockedModel.count } returns FIVE_INT
-
-        secondaryViewModel.getLiveData().observeForever(mockedObserver)
-        secondaryViewModel.init()
-
-        verify { mockedObserver.onChanged(capture(list)) }
-
-        assertEquals(INIT, list[0].state)
-        assertEquals(FIVE_STRING, list[0].count)
-    }
-
     @Test
     fun `ViewModel initialization test`() {
-        val list = arrayListOf<KotlinCounterViewModel.KotlinCounterData>()
+        val emmitedResultList = mutableListOf<KotlinCounterViewModel.KotlinCounterData>()
 
         viewModel.getLiveData().observeForever(mockedObserver)
-        viewModel.init()
+        viewModel.init(dataBase)
 
-        verify { mockedObserver.onChanged(capture(list)) }
+        verify { mockedObserver.onChanged(capture(emmitedResultList)) }
 
-        assertEquals(INIT, list[0].state)
-        assertEquals(ZERO_STRING, list[0].count)
+        assertEquals(INIT, emmitedResultList[0].state)
+        assertEquals(ZERO_STRING, emmitedResultList[0].count)
     }
 
     @Test
     fun `ViewModel set plus one test`() {
-        viewModel.init()
+        viewModel.init(dataBase)
 
-        // We can omit this step, depending if we are expecting more than one value to be posted from the ViewModel
-        // in this case we are expecting two posts so we need this for catching them
-        val list = arrayListOf<KotlinCounterViewModel.KotlinCounterData>()
+        // Here we use the UnconfinedTestDispatcher for executing coroutines.
+        runTest(UnconfinedTestDispatcher()) {
 
-        // Here we attach our mockedObserver to the ViewModel live data
-        viewModel.getLiveData().observeForever(mockedObserver)
-        // We use runBlocking to execute the coroutine, and since we are using a delay for simulation purposes,
-        // we need to add a .join to the sentence to make it wait for the coroutine to finish.
-        runBlocking {
+            // We can omit this step, depending if we are expecting more than one value to be posted from the ViewModel
+            // in this case we are expecting two posts so we need this for catching them
+            val emmitedResultList = mutableListOf<KotlinCounterViewModel.KotlinCounterData>()
+
+            // Here we attach our mockedObserver to the ViewModel live data
+            viewModel.getLiveData().observeForever(mockedObserver)
+
+            // we need to add a .join to the sentence to make it wait for the coroutine to finish.
             viewModel.setPlusOne().join()
+
+            // With this .onChanged() we capture the posted value in our list.
+            coVerify { mockedObserver.onChanged(capture(emmitedResultList)) }
+
+            // And here we verify that the posted values are what we expected.
+            assertEquals(INIT, emmitedResultList[0].state)
+            assertEquals(ZERO_STRING, emmitedResultList[0].count)
+            assertEquals(UPDATE_VALUE, emmitedResultList[1].state)
+            assertEquals(ONE_STRING, emmitedResultList[1].count)
         }
-
-        // With this .onChanged() we capture the posted value in our list.
-        verify { mockedObserver.onChanged(capture(list)) }
-
-        // And here we verify that the posted values are what we expected.
-        assertEquals(INIT, list[0].state)
-        assertEquals(ZERO_STRING, list[0].count)
-        assertEquals(UPDATE_VALUE, list[1].state)
-        assertEquals(ONE_STRING, list[1].count)
     }
 
     @Test
     fun `ViewModel set minus one test`() {
-        viewModel.init()
+        viewModel.init(dataBase)
 
-        val list = arrayListOf<KotlinCounterViewModel.KotlinCounterData>()
+        runTest(UnconfinedTestDispatcher()) {
+            val emmitedResultList = mutableListOf<KotlinCounterViewModel.KotlinCounterData>()
 
-        viewModel.getLiveData().observeForever(mockedObserver)
-        runBlocking {
+            viewModel.getLiveData().observeForever(mockedObserver)
+
             viewModel.setPlusOne().join()
             viewModel.setMinusOne().join()
+
+            coVerify { mockedObserver.onChanged(capture(emmitedResultList)) }
+
+            assertEquals(INIT, emmitedResultList[0].state)
+            assertEquals(ZERO_STRING, emmitedResultList[0].count)
+            assertEquals(UPDATE_VALUE, emmitedResultList[1].state)
+            assertEquals(ONE_STRING, emmitedResultList[1].count)
+            assertEquals(UPDATE_VALUE, emmitedResultList[2].state)
+            assertEquals(ZERO_STRING, emmitedResultList[2].count)
         }
-
-        verify { mockedObserver.onChanged(capture(list)) }
-
-        assertEquals(INIT, list[0].state)
-        assertEquals(ZERO_STRING, list[0].count)
-        assertEquals(UPDATE_VALUE, list[1].state)
-        assertEquals(ONE_STRING, list[1].count)
-        assertEquals(UPDATE_VALUE, list[2].state)
-        assertEquals(ZERO_STRING, list[2].count)
     }
 
     @Test
     fun `ViewModel reset counter test`() {
-        viewModel.init()
-        val list = arrayListOf<KotlinCounterViewModel.KotlinCounterData>()
+        viewModel.init(dataBase)
 
-        viewModel.getLiveData().observeForever(mockedObserver)
+        runTest(UnconfinedTestDispatcher()) {
+            val emmitedResultList = arrayListOf<KotlinCounterViewModel.KotlinCounterData>()
 
-        runBlocking {
-            viewModel.setPlusOne().join()
-            viewModel.setReset().join()
+            viewModel.getLiveData().observeForever(mockedObserver)
+
+            runBlocking {
+                viewModel.setPlusOne().join()
+                viewModel.setReset().join()
+            }
+
+            coVerify { mockedObserver.onChanged(capture(emmitedResultList)) }
+
+            assertEquals(INIT, emmitedResultList[0].state)
+            assertEquals(ZERO_STRING, emmitedResultList[0].count)
+            assertEquals(UPDATE_VALUE, emmitedResultList[1].state)
+            assertEquals(ONE_STRING, emmitedResultList[1].count)
+            assertEquals(UPDATE_VALUE, emmitedResultList[2].state)
+            assertEquals(ZERO_STRING, emmitedResultList[2].count)
         }
-
-        verify { mockedObserver.onChanged(capture(list)) }
-
-        assertEquals(INIT, list[0].state)
-        assertEquals(ZERO_STRING, list[0].count)
-        assertEquals(UPDATE_VALUE, list[1].state)
-        assertEquals(ONE_STRING, list[1].count)
-        assertEquals(UPDATE_VALUE, list[2].state)
-        assertEquals(ZERO_STRING, list[2].count)
     }
 
     companion object {
         const val ZERO_STRING = "0"
         const val ONE_STRING = "1"
-        const val FIVE_STRING = "5"
         const val ZERO_INT = 0
-        const val FIVE_INT = 5
     }
 }
